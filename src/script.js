8ac7bd629ba4712a02a7e0433df49a0927eab501
Olesia Libra <98489034+Solymar1388@users.noreply.github.com>
2022-07-22 20:03:02 +0300

    Added relevant icon from API

--- src/script.js
+++ src/script.js
@@ -30,19 +30,28 @@ function displayTemperature(response) {
     let cityElement = document.querySelector('#city');
     let descriptionElement = document.querySelector('#description');
     let humidityElement = document.querySelector('#humidity');
-    let windElement = document.querySelector('#wind')
-    let dateElement = document.querySelector('#date')
-    temperatureElement.innerHTML = Math.round(response.data.main.temp)
-    cityElement.innerHTML = response.data.name
-    descriptionElement.innerHTML = response.data.weather[0].description
-    humidityElement.innerHTML = response.data.main.humidity
-    windElement.innerHTML = Math.round(response.data.wind.speed)
-    dateElement.innerHTML = formatDate(response.data.dt * 1000)
+    let windElement = document.querySelector('#wind');
+    let dateElement = document.querySelector('#date');
+    let iconElement = document.querySelector("#icon");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute(
+        'src',
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    iconElement.setAttribute(
+        'alt', 
+        response.data.weather[0].description);
 }
 
 
 let apiKey = "ce41b28ce42cc32953b2215b8a1bd7b5";
-let apiUrl ='https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=ce41b28ce42cc32953b2215b8a1bd7b5&units=metric';
+let city = "Kyiv";
+let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature)
 

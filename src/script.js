1cdffff9513a2ec52da4b6f98404921713c73943
Olesia Libra <98489034+Solymar1388@users.noreply.github.com>
2022-07-22 22:24:27 +0300

    Implemented the unit convert

--- src/script.js
+++ src/script.js
@@ -33,6 +33,9 @@ function displayTemperature(response) {
     let windElement = document.querySelector('#wind');
     let dateElement = document.querySelector('#date');
     let iconElement = document.querySelector("#icon");
+
+    celciusTemperature = response.data.main.temp;
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
@@ -55,13 +58,40 @@ axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event) {
-  event.preventDefault();
-  let cityInputElement = document.querySelector('#city-input');
-  search(cityInputElement.value);
+    event.preventDefault();
+    let cityInputElement = document.querySelector('#city-input');
+    search(cityInputElement.value);
 }
 
-search("Kyiv")
+function showFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector('#temperature');
+    celciusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function showCelciusTemperature(event) {
+    event.preventDefault();
+    celciusLink.classList.add('active');
+    fahrenheitLink.classList.remove('active');
+    let temperatureElement = document.querySelector('#temperature');
+    temperatureElement.innerHTML = Math.round(celciusTemperature);
+}
+
+let celciusTemperature = null;
+
 
 let form = document.querySelector('#search-form');
-form.addEventListener('submit', handleSubmit)
+form.addEventListener('submit', handleSubmit);
 
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
+
+let celciusLink = document.querySelector('#celcius-link');
+celciusLink.addEventListener('click', showCelciusTemperature);
+
+
+search('Kyiv');

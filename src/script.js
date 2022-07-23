0f0ccb667349b91a62c1375a766a1a18325b7797
Olesia Libra <98489034+Solymar1388@users.noreply.github.com>
2022-07-23 15:22:42 +0300

    Added some forecast JS/CSS

--- src/script.js
+++ src/script.js
@@ -23,6 +23,33 @@ function formatDate(timestamp) {
     return `${day}, ${hours}:${minutes}`
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast")
+
+    let days = ['Thu', 'Fri', 'Sat', 'Sun'];
+    
+    let forecastHTML = `<div class="row">`;
+
+    days.forEach(function(day) {
+    forecastHTML = forecastHTML + `
+            <div class="col-2">
+                <div class="weather-forecast-date">
+                    ${day}
+                </div>
+                    <img src="http://openweathermap.org/img/wn/01d@2x.png" 
+                        alt=""
+                        width="42">
+                <div class="weather-forecast-temperatures">
+                    <span class="weather-forecast-temperature-max">18°</span>
+                    <span class="weather-forecast-temperature-min">12°</span>     
+                </div>
+            </div>
+        `;
+    })
+
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
 
 function displayTemperature(response) {
 
@@ -83,6 +110,8 @@ function showCelciusTemperature(event) {
 let celciusTemperature = null;
 
 
+
+
 let form = document.querySelector('#search-form');
 form.addEventListener('submit', handleSubmit);
 
@@ -95,3 +124,4 @@ celciusLink.addEventListener('click', showCelciusTemperature);
 
 
 search('Kyiv');
+displayForecast()
